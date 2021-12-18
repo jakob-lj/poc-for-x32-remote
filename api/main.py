a91b725b8f181c36d32ba4ebdbb1b474d1f52a0f
Jakob Lund Johannessen <jakob-lj@hotmail.com>
2021-12-18 15:20:01 +0100

    feat: use x32 on 18. des

--- api/main.py
+++ api/main.py
@@ -9,21 +9,24 @@ class RequestSoundChannel(BaseModel):
     channel: str
     muteValue: int
 
+
 def send(channel, muteValue):
     from pythonosc.udp_client import SimpleUDPClient
 
-    ip = "host.docker.internal"
+    ip = "192.168.1.167"
     port = 10023
 
     client = SimpleUDPClient(ip, port)  # Create client
 
     client.send_message("/ch/%s/mix/on" % channel, muteValue)
 
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
+
 @app.post("/sound")
 async def setMute(options: RequestSoundChannel):
     send(options.channel, options.muteValue)
-    return {"message":"ok"}
+    return {"message": "ok"}
